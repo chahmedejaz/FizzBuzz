af46070068bbfe8bfcd37ea29308b291804ba230
Ahmed Ijaz <chahmedejaz9050@gmail.com>
2023-07-09 17:36:34 +0500

    add Fizz & Buzz Rules tests

---
+++ FizzBuzz.Tests/BuzzRuleTests.cs
@@ -0,0 +1,45 @@
+﻿namespace FizzBuzz.Tests
+{
+    public class BuzzRuleTests : IFizzBuzzRuleTest
+    {
+        BuzzRule buzzRule;
+        public BuzzRuleTests()
+        {
+            buzzRule = new BuzzRule();
+        }
+
+        [Fact]
+        public void BuzzRule_ShouldMatchDivisor()
+        {
+            Assert.True(ShouldMatchDivisor(5));
+        }
+
+        [Fact]
+        public void BuzzRule_ShouldNotMatchDivisor()
+        {
+            Assert.False(ShouldMatchDivisor(10));
+        }
+
+        [Fact]
+        public void BuzzRule_ShouldMatch()
+        {
+            Assert.True(ShouldMatch(10));
+        }
+
+        [Fact]
+        public void BuzzRule_ShouldNotMatch()
+        {
+            Assert.False(ShouldMatch(11));
+        }
+
+        public bool ShouldMatch(int number)
+        {
+            return buzzRule.IsMatch(number);
+        }
+
+        public bool ShouldMatchDivisor(int divisor)
+        {
+            return divisor == Constants.BuzzDivisor;
+        }
+    }
+}
---
+++ FizzBuzz.Tests/IFizzBuzzRuleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+    internal interface IFizzBuzzRuleTest
+    {
+        bool ShouldMatch(int number);
+        bool ShouldMatchDivisor(int divisor);
+    }
+}
